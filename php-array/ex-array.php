a13cd316c38bd08ae8893213dc6ef3a725a717c4
Fatima Arellano <41347973+fatimaarellano@users.noreply.github.com>
2018-12-03 19:06:43 -0500

    Add files via upload

---
+++ php-array/ex-array.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<head>
+  <title>Array Assigment</title>
+</head>
+
+<body>
+  <?php
+
+$a = array(0,1,2,3,4);
+
+$a[0] = "One";
+$a[1] = "Two";
+$a[2] = "Three";
+$a[3] = "Four";
+$a[4] = "Five";
+
+echo "Extract the number $a[3\n]";
+echo '<br>';
+
+?>
+</body>
---
+++ php-array/ex-array2.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<head>
+  <title>Array Assigment2</title>
+</head>
+
+<body>
+  <?php
+
+  $a = array(0,1,2,3,4,5,6);
+  echo "Sum= "; . array_sum($a) . "\n";
+
+  ?>
+</body>
